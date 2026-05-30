42f2969ccdae36a1b1f854d4c564390e5dd6fe15
inggarr <muhammad.inggar.as@gmail.com>
2026-05-30 16:50:21 +0100

    update

--- web-dinamis/config/koneksi.php
+++ web-dinamis/config/koneksi.php
@@ -2,7 +2,7 @@
 
 // Baca dari environment variable (production/Docker)
 // Jika tidak ada, fallback ke nilai lokal (development)
-$host = getenv('DB_HOST') ?: 'komdis-db';
+$host = getenv('DB_HOST') ?: 'dbkomdis';
 $user = getenv('DB_USER') ?: 'userkomdis';
 $pass = getenv('DB_PASS') ?: '12345';
 $db = getenv('DB_NAME') ?: 'dbkomdis';
